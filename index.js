e9d79b23419045aec723b33ae6ddb2ae2f05ecce
jplante01 <90069927+jplante01@users.noreply.github.com>
2023-07-30 10:33:06 -0700

    Create server

---
+++ index.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const fs = require('fs');
+const port = 5000;
+
+http
+  .createServer((req, res) => {
+    if (req.url === '/') {
+      fs.readFile('index.html', 'utf8', (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Internal Server Error');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data);
+        }
+      });
+    } else if (req.url === '/about') {
+      fs.readFile('about.html', 'utf8', (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Internal Server Error');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data);
+        }
+      });
+    } else if (req.url === '/contact-me') {
+      fs.readFile('contact-me.html', 'utf8', (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Internal Server Error');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data);
+        }
+      });
+    } else {
+      fs.readFile('404.html', 'utf8', (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Internal Server Error');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data);
+        }
+      });
+    }
+  })
+  .listen(port, () => console.log(`Listening on port ${port}`));
